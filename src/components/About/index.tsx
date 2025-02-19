f93502e611b78cb15d046d7c9f9c5d882f551dc9
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-02-19 09:47:06 -0300

    última aula state e useLocation

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,6 +1,11 @@
-export const About  = () => {
-  return ( 
+import { useLocation } from "react-router-dom";
+
+export const About = () => {
+  const { state } = useLocation()
+  return (<>
     <h1>Sobre</h1>
-   );
+    <p>{state}</p>
+  </>
+  );
 }
- 
+
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -6,7 +6,7 @@ export const Navbar = () => {
     <nav className="menu">
       <ul>
         <li><Link to={"/"}>Home</Link></li>
-        <li><Link to={"/about"}>Sobre</Link></li>
+        <li><Link to={"/about"}  state={'This is state from Abou'}>Sobre</Link></li>
         <li><Link to={"/redirect"}>Redirect</Link></li>
         <li><Link to={"/posts"}>Posts</Link></li>
         <li><Link to={"/posts/10"}>Post</Link></li>
