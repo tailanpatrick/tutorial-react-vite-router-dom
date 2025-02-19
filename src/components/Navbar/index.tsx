f6f88e4a266df3dec78c125949d68630139b6804
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-02-19 09:27:35 -0300

    aula navigate e useNavigate

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -7,6 +7,7 @@ export const Navbar = () => {
       <ul>
         <li><Link to={"/"}>Home</Link></li>
         <li><Link to={"/about"}>Sobre</Link></li>
+        <li><Link to={"/redirect"}>Redirect</Link></li>
         <li><Link to={"/posts"}>Posts</Link></li>
         <li><Link to={"/posts/10"}>Post</Link></li>
       </ul>
--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { About } from './components/About'
 import { Navbar } from './components/Navbar'
 import { Post } from './components/Post'
+import { Redirect } from './components/Redirect'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -17,6 +18,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path='/about' element={<About/>}/>
         <Route path='/posts/:id' element={<Post/>}/>
         <Route path='/posts' element={<Post/>}/>
+        <Route path='/redirect' element={<Redirect/>}/>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
